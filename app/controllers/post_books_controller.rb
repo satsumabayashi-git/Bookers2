ce8c1bd18ac38e1540151c1b717f85dea336d578
satsumabayashi-git <209392348+satsumabayashi-git@users.noreply.github.com>
2025-08-06 01:32:49 +0000

    [Add] book_detail-view

--- app/controllers/post_books_controller.rb
+++ app/controllers/post_books_controller.rb
@@ -2,10 +2,11 @@ class PostBooksController < ApplicationController
 
   def index
     @post_books = PostBook.all
-    @post_book = PostBook.new
+    @post_book_new = PostBook.new
   end
 
   def show
+    @post_book_new = PostBook.new
     @post_book = PostBook.find(params[:id])
   end
 
@@ -13,6 +14,12 @@ class PostBooksController < ApplicationController
     @post_book = PostBook.new(post_book_params)
     @post_book.user_id = current_user.id
     @post_book.save
+    redirect_to post_books_path
+  end
+
+  def destroy
+    @post_book = PostBook.find(params[:id])
+    @post_book.destroy
     redirect_to post_books_path
   end
 
