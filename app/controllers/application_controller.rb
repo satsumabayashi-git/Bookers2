5b8a7e7345e09f3a850f984127ae0a912fcb2b7f
EC2 Default User <ec2-user@ip-172-31-32-156.ap-northeast-1.compute.internal>
2025-08-13 08:23:10 +0000

    [Add] validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def after_sign_in_path_for(resource)
-    about_path
+    user_path(current_user.id)
   end
 
   def after_sign_out_path_for(resource)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,15 +15,25 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to books_path
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to books_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
